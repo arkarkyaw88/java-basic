fc91b157468fcaf48b80dbef9e9bce793647e65e
ArKar Kyaw <39894962+ElectroArmy@users.noreply.github.com>
2023-01-10 14:50:46 +0630

    Create Methods.java
    Methods

---
+++ Methods.java
@@ -0,0 +1,36 @@
+/**
+  * Author  : ARKAR KYAW
+  * Created : 8 Jan 2023 Sunday
+  * Master  : Min Lwin ( JDC )
+  * Class   : Java Basic Online Class
+  * Define  : Methods
+  */
+
+public class Methods {
+  
+  public static void main(String[] args) {
+    
+  }
+  // Return Type
+  // Method Name
+  // Arguments
+  // Method Body
+  
+  void hello() {
+    System.out.println("Hello Dude!");
+  }
+  
+  void hello(String name) {}
+  
+  void hello(String name, int count) {}
+  
+  void hello(String ... names) {}
+  
+  
+  // Methods Name + Arguments List = Method Signature
+  
+  int add(int a, int b) {
+    return '0';
+  }
+
+}
