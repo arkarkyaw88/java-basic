e86edc90361574848db37a6efa563e7c6c926395
ArKar Kyaw <39894962+ElectroArmy@users.noreply.github.com>
2023-01-13 08:33:58 +0630

    Constructor in java

---
+++ Constructor.java
@@ -0,0 +1,39 @@
+/**
+  * Author  : ARKAR KYAW
+  * Created : 9 Jan 2023 Monday
+  * Master  : Min Lwin ( JDC )
+  * Class   : Java Basic Online Class
+  * Define  : Constructor
+  */
+
+
+public class Constructor {
+
+  public static void main(String[] args) {
+    
+    //Var assign
+    Hello hello;
+    
+    //Create an object for invoke another methods
+    hello = new Hello();
+    
+    //Assigned ownername dot method can invoke
+    hello.greet();
+    
+  
+  }
+
+}
+
+class Hello {
+  
+  //Constructor
+  Hello() {
+    System.out.println("Constructor method invoked.");
+  }
+  
+  //Method
+  void greet() {
+    System.out.println("Hello Java User");
+  }
+}
