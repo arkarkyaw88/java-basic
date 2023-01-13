c53815caed02a4ee41f970c708ea945c274e4ae3
ArKar Kyaw <39894962+ElectroArmy@users.noreply.github.com>
2023-01-13 16:01:05 +0630

    Create Hello.java

---
+++ hello/Hello.java
@@ -0,0 +1,14 @@
+/**
+  * Author  : ARKAR KYAW
+  * Created : 13 Jan 2023 Friday
+  * Master  : Min Lwin ( JDC )
+  * Class   : Java Basic Online Class
+  * Define  : Package
+  */
+
+//Top Level Code
+package hello;
+
+public class Hello {
+  System.out.println("Hello Package");
+}
