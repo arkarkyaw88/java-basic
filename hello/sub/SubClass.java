40f6413efad16dcd1e94015d1d46930472d6f757
ArKar Kyaw <39894962+ElectroArmy@users.noreply.github.com>
2023-01-15 10:27:03 +0630

    Create SubClass.java

---
+++ hello/sub/SubClass.java
@@ -0,0 +1,12 @@
+//Top Level Code
+package hello.sub;
+
+//Folder is important
+//For Naming rules and for revoke
+public class SubClass {
+  
+  //Method of greet
+  public void greet() {
+    System.out.println("Message from Sub Class");
+  }
+}
