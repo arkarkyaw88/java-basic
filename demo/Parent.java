f1300f404f61ffb7f7c3c4b4bdcce86e7d874415
ArKar Kyaw <39894962+ElectroArmy@users.noreply.github.com>
2023-01-17 14:27:30 +0630

    Create Parent.java

---
+++ demo/Parent.java
@@ -0,0 +1,16 @@
+import demo;
+
+public class Parent {
+  
+  static {
+    System.out.println("Static Block of Parent");
+  }
+  
+  {
+    System.out.println("Instance Block of Parent");
+  }
+  
+  public Parent() {
+    System.out.println("Constructor of Parent");
+  }
+}
