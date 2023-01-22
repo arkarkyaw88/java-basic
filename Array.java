5ea3d06210434596ce453b06c27d193427c5b303
ArKar Kyaw <39894962+ElectroArmy@users.noreply.github.com>
2023-01-22 14:33:47 +0630

    Creating an array object.

---
+++ Array.java
@@ -0,0 +1,26 @@
+//Create an array object
+//Styles
+
+String [] array;
+
+int array [];
+
+//If we know final data, just declare like this
+int array [] = {1, 2, 3};
+//Invoke
+array;
+
+//If we add new integer to existing array like this, it'll cause error
+array = {4, 5, 6};
+
+//True Way
+array = new int[]{4, 5, 6};
+
+//Create 5 null array object
+String [] strs = new String[5];
+
+Date [] dates = new Date[5];
+
+int [] ints = new int[5];
+
+double [] ints = new double[5];
