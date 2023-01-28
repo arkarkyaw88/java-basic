f1827f7c7485ccd76ecc0925c2298073a6859337
ArKar Kyaw <39894962+ElectroArmy@users.noreply.github.com>
2023-01-28 14:24:36 +0630

    Update Student.java

--- packages/com/jdc/demo/dto/Student.java
+++ packages/com/jdc/demo/dto/Student.java
@@ -1,4 +1,29 @@
+package com.jdc.demo.dto;
 
 public class Student {
+  
+  private int id;
+  private final String name;
+  private final String phone;
+  private final String email;
+  
+  public Student(String name, String phone, String email) {
+  
+    this.name;
+    this.phone;
+    this.email;
+  }
+  
+  public String getName() {
+    return name;
+  }
+  
+  public String getPhone() {
+    return phone;
+  }
+  
+  public String getEmail() {
+    return email;
+  }
   
 }
