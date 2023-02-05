bdaf8226ec0bc446c5a29699fd1523e229a8f39b
ArKar Kyaw <39894962+ElectroArmy@users.noreply.github.com>
2023-02-05 14:22:10 +0630

    Update Main.java
    // Run outside from folder path ..
    // javac package_import_test/Main.java

--- os-01-weekend/package_import_test/Main.java
+++ os-01-weekend/package_import_test/Main.java
@@ -1,1 +1,24 @@
+//Top Level Code
+package package_import_test;
 
+import package_import_test.folderOne.Student;
+
+//For Scanner
+import java.util.Scanner;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		//Create an Obj for use another class's method
+		Student st = new Student();
+		//Invoke
+		st.showName();
+	}
+}
+
+// Run outside from folder path ..
+// javac package_import_test/Main.java
