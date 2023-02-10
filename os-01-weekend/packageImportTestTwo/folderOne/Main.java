9d0f0ddbde7535ce338648c64a7970f07c806d01
ArKar Kyaw <39894962+ElectroArmy@users.noreply.github.com>
2023-02-10 10:11:45 +0630

    Update Main.java

--- os-01-weekend/packageImportTestTwo/folderOne/Main.java
+++ os-01-weekend/packageImportTestTwo/folderOne/Main.java
@@ -1,1 +1,16 @@
 //Main
+package packageImportTestTwo.folderOne;
+
+import packageImportTestTwo.folderTwo.Data;
+import packageImportTestTwo.folderThree.Utils;
+
+public class Main {
+  
+  public static void main(String [] args) {
+    
+    Data d = new Data();
+    d.showData(5);
+  
+  }
+
+}
