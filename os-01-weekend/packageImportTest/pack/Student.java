66b1ffb79c45d848749ce4d0739347bb9218e297
ArKar Kyaw <39894962+ElectroArmy@users.noreply.github.com>
2023-02-10 09:39:57 +0630

    Update Student.java

--- os-01-weekend/packageImportTest/pack/Student.java
+++ os-01-weekend/packageImportTest/pack/Student.java
@@ -1,1 +1,9 @@
+package packageImportTest.pack;
 
+public class Student {
+  
+  public String defaultName;
+  private String privateName;
+  protected String protectedName;
+  String defaultName;
+}
