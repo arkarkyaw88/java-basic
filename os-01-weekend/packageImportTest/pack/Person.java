9ec1d427d8aa5a80fdb8ccc8a51ddecb7805f0e7
ArKar Kyaw <39894962+ElectroArmy@users.noreply.github.com>
2023-02-10 09:38:24 +0630

    Update Person.java

--- os-01-weekend/packageImportTest/pack/Person.java
+++ os-01-weekend/packageImportTest/pack/Person.java
@@ -1,1 +1,15 @@
 //Person
+package packageImportTest.pack;
+
+public class Person {
+  
+  public void getStudent() {
+    
+    Student s = new Student();
+    s.defaultName = "Default";
+    s.publicName = "Public";
+    s.privateName = "Private";
+    s.protectedName = "Protected";
+    
+  }
+}
