4b3b7ce9ef808c3af82aed82262c49481f4d0715
ArKar Kyaw <39894962+ElectroArmy@users.noreply.github.com>
2023-02-10 09:44:36 +0630

    Update Main.java

--- os-01-weekend/packageImportTest/Main.java
+++ os-01-weekend/packageImportTest/Main.java
@@ -1,12 +1,14 @@
-//Main
+// Declare folder path inside
 package packageImportTest;
-
+// Declare package path 
 import packageImportTest.pack.*;
 
 public class Main {
   
   public static void main(String [] args) {
-  
-  
+    // Create an obj for use /pack/Person Class
+    Person p = new Person();
+    // Obj dot methodName invoke
+    p.getStudent();
   }
 }
