379f146b49a3568a2be039f474fbe576797cbc53
ArKar Kyaw <39894962+ElectroArmy@users.noreply.github.com>
2023-02-17 20:12:50 +0630

    Update CheckStringTest.java

--- os-01-weekend/secondWeek/dayTwo/Assignment/assignOne/CheckStringTest.java
+++ os-01-weekend/secondWeek/dayTwo/Assignment/assignOne/CheckStringTest.java
@@ -1,4 +1,24 @@
 public static class CheckStringTest {
   
+  public static void main(String [] args) {
+    
+    test("[]", true);
+    test("[}", false);
+    test("[]{}", true);
+    test("[()]", true);
+    test("[(){}]", true);
+    test("[()", false);
+    test("[(]", false);
+  }
+  
+  static void test(String str, boolean expected) {
+  
+    boolean result = CheckString.isBalance(str);
+    
+    System.out.println(
+      "Input: \"%-8s\", Expected: %-6s, Result: %-6s (%s) %n",
+      str, expected, result, (expected == result) ? "OK" : "NG"
+    );
+  }
   
 }
